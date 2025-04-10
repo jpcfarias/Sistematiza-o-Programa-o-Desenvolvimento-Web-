2b7952692410078e297ed72e336a4d98a007cd6c
Joao Farias <j.p.fariasolhoverde@gmail.com>
2025-04-10 18:17:25 -0300

    Add files via upload

---
+++ api-profissionais-saude/run.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Iniciando a API de Profissionais de Saude...
+java -jar target/api-profissionais-saude-1.0.0.jar
+pause 
---
+++ api-profissionais-saude/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Iniciando a API de Profissionais de Saude..."
+java -jar target/api-profissionais-saude-1.0.0.jar 
